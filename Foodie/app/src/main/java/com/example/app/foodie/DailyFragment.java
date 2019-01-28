bffcabea55c4b360fd784f972dddd3f07df9cf0c
sbeheraeie@gmail.com <sbeheraeie@gmail.com>
2019-01-28 13:04:31 -0800

    dharitri_29

--- Foodie/app/src/main/java/com/example/app/foodie/DailyFragment.java
+++ Foodie/app/src/main/java/com/example/app/foodie/DailyFragment.java
@@ -1,5 +1,6 @@
 package com.example.app.foodie;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,12 +8,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.sukanta.foodie.R;
 
+import java.util.Calendar;
+
 
 public class DailyFragment extends Fragment {
-
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
     public DailyFragment() {
         // Required empty public constructor
@@ -35,7 +40,34 @@ public class DailyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daily, container, false);
+        View v= inflater.inflate(R.layout.fragment_daily, container, false);
+        final EditText startEd=(EditText)v.findViewById(R.id.startEd);
+        EditText endEd=(EditText)v.findViewById(R.id.endEd);
+
+        startEd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                startEd.setText(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+        return v;
     }
 
 
